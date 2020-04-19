196512d6933986749ad3f704046c3ad2db460c1d
PHastings37 <patrickhastings@hotail.co.uk>
2020-04-19 16:11:00 +0100

    finished single push

--- project 2.py
+++ project 2.py
@@ -44,16 +44,19 @@ def euler(m, k, b, nsteps, xe, ve):
     
     f.close()
     
+
     plt.figure()
     plt.title('euler')
-    plt.plot(xe, 'b')
-    plt.plot(ve, 'r')
+    plt.plot(xe, 'b', label='position')
+    plt.plot(ve, 'r', label='velocity')
+    plt.legend()
     plt.show()
-
+ 
     plt.figure()
     plt.title('euler')
     plt.plot(xe, ve, 'b')
     plt.show()
+
 
 
 
@@ -78,13 +81,15 @@ def improv_euler(m, k, b, nsteps, xie, vie):
     df.to_csv('ImprovedEulerData.csv')
     
     f.close()
-    
+
+      
     plt.figure()
     plt.title('Ieuler')
-    plt.plot(xie, 'b')
-    plt.plot(vie, 'r')
+    plt.plot(xie, 'b', label='position')
+    plt.plot(vie, 'r', label='velocity')
+    plt.legend()
     plt.show()
-
+  
     plt.figure()
     plt.title('Ieuler')
     plt.plot(xie, vie, 'b')
@@ -96,9 +101,9 @@ def verlet(m, k, b, nsteps, xv, vv):
     D = 2 * m + b * h
     A = 2 * ((2 * m - k * h ** 2) / D)
     B = (b * h - 2 * m) / D
-
+    a_nought = -(k / m) *xv[0] - (b / m) * vv[0]
     # use euler for first step
-    xv[1] = xv[0] + (h * vv[0])
+    xv[1] = xv[0] + (h * vv[0]) + 0.5 * h**2 * a_nought
 
     for i in range(nsteps - 2):
         xv[i + 2] = A * xv[i + 1] + B * xv[i]
@@ -116,16 +121,19 @@ def verlet(m, k, b, nsteps, xv, vv):
     
     f.close()
     
+
     plt.figure()
     plt.title('Verlet')
-    plt.plot(xv, 'b')
-    plt.plot(vv, 'r')
+    plt.plot(xv, 'b', label='position')
+    plt.plot(vv, 'r', label='velocity')
+    plt.legend()
     plt.show()
-
+ 
     plt.figure()
     plt.title('Verlet')
     plt.plot(xv, vv, 'b')
     plt.show()
+ 
 
 
 
@@ -148,19 +156,21 @@ def euler_cromer(m, k, b, nsteps, xec, vec):
     df.to_csv('EulerCromerData.csv')
     
     f.close()
-    
+
+      
     plt.figure()
     plt.title('EC')
-    plt.plot(xec, 'b')
-    plt.plot(vec, 'r')
+    plt.plot(xec, 'b', label='position')
+    plt.plot(vec, 'r', label='velocity')
+    plt.legend()
     plt.show()
-
+  
     plt.figure()
     plt.title('EC')
     plt.plot(xec, vec, 'b')
     plt.show()
-
-
+  
+ 
 
 def analytical(m, b, h, k, T):
     gamma = b / m
@@ -201,17 +211,20 @@ def analytical(m, b, h, k, T):
     df.to_csv('AnalyticalData.csv')
     
     f.close()
-    
+
+     
     plt.figure()
     plt.title('Analytical')
-    plt.plot(xa, 'b')
-    plt.plot(va, 'r')
+    plt.plot(xa, 'b', label='position')
+    plt.plot(va, 'r', label='velocity')
+    plt.legend()
     plt.show()
-
+ 
     plt.figure()
     plt.title('Analytical')
     plt.plot(xa, va, 'b')
     plt.show()
+
 
 
 # user inputs of physical parameters
@@ -219,7 +232,8 @@ def analytical(m, b, h, k, T):
 
 m = 3.82
 k = 0.61
-b = 0.9
+b = 0.1
+F = -20
 # valid = False
 #
 # while valid == False:
@@ -236,7 +250,7 @@ b = 0.9
 # length of integration
 T = 221
 # step size
-h = 1
+h = 0.2
 # num of steps (i) needs to be an int
 nsteps = int(T / h)
 
@@ -305,7 +319,6 @@ plt.legend()
 plt.show()
 
 
-#####################################################################################
 """
 Finding the best method
 """
@@ -328,33 +341,148 @@ for i in range(len(energy_diffs)):
         
 
 if pos == 0:
+    xe = np.zeros(nsteps)
+    ve = np.zeros(nsteps)
     print('The most accurate method is Euler')
     euler(m, k, np.sqrt(k * m), nsteps, x, v)
     
     euler(m, k, 2 * np.sqrt(k * m), nsteps, x, v)
     
     euler(m, k, 4 * np.sqrt(k * m), nsteps, x, v)
+    
+    for i in range(nsteps - 1):
+        # calculate a at ste i
+        # no array of these as these are not needed for later
+        if i < (nsteps/2):
+            
+            ae = -(k / m) * xe[i] - (b / m) * ve[i]
+            # add next value for x and v
+            xe[i + 1] = xe[i] + ve[i] * h
+            ve[i + 1] = ve[i] + ae * h
+        else:
+             ae = -(k / m) * xe[i] - (b / m) * ve[i] + F/m
+             # add next value for x and v
+             xe[i + 1] = xe[i] + ve[i] * h
+             ve[i + 1] = ve[i] + ae * h
+        
+    plt.figure()     
+    plt.title('Forced euler')
+    plt.plot(xe, 'b', label='position')
+    plt.plot(ve, 'r', label='velocity')
+    plt.legend()
+    plt.show()
+ 
+    plt.figure()
+    plt.title(' Forced euler')
+    plt.plot(xe, ve, 'b')
+    plt.show()
+    
 elif pos == 1:
+    xie = np.zeros(nsteps)
+    vie = np.zeros(nsteps)
     print('The most accurate method is Improved Euler')
     improv_euler(m, k, np.sqrt(k * m), nsteps, x, v)
     
     improv_euler(m, k, 2 * np.sqrt(k * m), nsteps, x, v)
     
     improv_euler(m, k, 4 * np.sqrt(k * m), nsteps, x, v)
+    
+    for i in range(nsteps - 1):
+        # calculate a at ste i
+        # no array of these as these are not needed for later
+        if i < (nsteps/2):
+            aie = -(k / m) * xie[i] - (b / m) * vie[i]
+            # add next value for x and v
+            xie[i + 1] = xie[i] + vie[i] * h + 0.5 * h ** 2 * aie
+            vie[i + 1] = vie[i] + aie * h
+        else:
+            aie = -(k / m) * xie[i] - (b / m) * vie[i] + F/m
+            # add next value for x and v
+            xie[i + 1] = xie[i] + vie[i] * h + 0.5 * h ** 2 * aie
+            vie[i + 1] = vie[i] + aie * h
+    plt.figure()     
+    plt.title('Forced Improved euler')
+    plt.plot(xie, 'b', label='position')
+    plt.plot(vie, 'r', label='velocity')
+    plt.legend()
+    plt.show()
+ 
+    plt.figure()
+    plt.title(' Forced Improved euler')
+    plt.plot(xie, vie, 'b')
+    plt.show()
 elif pos == 2:
+    xv = np.zeros(nsteps)
+    vv = np.zeros(nsteps)
+    xv[0] = 0
+    vv[0] = -1
+    
     print('The most accurate method is Verlet')
     verlet(m, k, np.sqrt(k * m), nsteps, x, v)
     
     verlet(m, k, 2 * np.sqrt(k * m), nsteps, x, v)
     
     verlet(m, k, 4 * np.sqrt(k * m), nsteps, x, v)
+    
+    D = 2 * m + b * h
+    A = 2 * ((2 * m - k * h ** 2) / D)
+    B = (b * h - 2 * m) / D
+    a_nought = -(k / m) *xv[0] - (b / m) * vv[0]
+    # use euler for first step
+    xv[1] = xv[0] + (h * vv[0]) + 0.5 * h**2 * a_nought
+
+    for i in range(nsteps - 2):
+        if i != (round(nsteps/2)):
+            xv[i + 2] = A * xv[i + 1] + B * xv[i]
+            vv[i + 1] = (xv[i + 2] - xv[i]) / (2 * h)
+        else:
+            xv[i + 2] = A * xv[i + 1] + B * xv[i] + ((2 * h**2) / D) * F
+            vv[i + 1] = (xv[i + 2] - xv[i]) / (2 * h)
+
+    
+    plt.figure()     
+    plt.title('Forced Verlet')
+    plt.plot(xv, 'b', label='position')
+    plt.plot(vv, 'r', label='velocity')
+    plt.legend()
+    plt.show()
+ 
+    plt.figure()
+    plt.title(' Forced Verlet')
+    plt.plot(xv, vv, 'b')
+    plt.show()
+        
 else:
+    xec = np.zeros(nsteps)
+    vec = np.zeros(nsteps)
     print('The most accurate method is Euler Cromer')
     euler_cromer(m, k, np.sqrt(k * m), nsteps, x, v)
     
     euler_cromer(m, k, 2 * np.sqrt(k * m), nsteps, x, v)
     
     euler_cromer(m, k, 4 * np.sqrt(k * m), nsteps, x, v)
+    
+    for i in range(nsteps - 1):
+        if i < (nsteps/2):
+            aec = -(k / m) * xec[i] - (b / m) * vec[i]
+            vec[i + 1] = vec[i] + aec * h
+            xec[i + 1] = xec[i] + h * vec[i + 1]
+        else:
+            aec = -(k / m) * xec[i] - (b / m) * vec[i] + F/m
+            vec[i + 1] = vec[i] + aec * h
+            xec[i + 1] = xec[i] + h * vec[i + 1]
+            
+    plt.figure()     
+    plt.title('Forced euler cromer')
+    plt.plot(xec, 'b', label='position')
+    plt.plot(vec, 'r', label='velocity')
+    plt.legend()
+    plt.show()
+ 
+    plt.figure()
+    plt.title(' Forced euler cromer')
+    plt.plot(xec, vec, 'b')
+    plt.show()
 
 
 
